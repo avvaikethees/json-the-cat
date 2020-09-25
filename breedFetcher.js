c081b4c01103a5eefe5da16030ae5a78b3b30796
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-09-25 00:13:57 +0000

    Wrote some unit test code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,25 +1,25 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
-let url = 'https://fsdgdapi.thecatapi.com/v1/breeds/search?q=';
-let argsString = args[0].toString();
-url = url.concat(argsString);
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.log('Failed to make a request: ', error);
-    return;
+const fetchBreedDescription = function(breedName, callback) {
+  let url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      const data = JSON.parse(body);
+      if (data[0] === undefined) {
+      callback(null, "Please enter valid breed")
+    } else {
+      console.log(data[0].description);
+    }
   }
+  });
+};
 
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log('Breed not found! Please enter a valid breed');
-  } else {
-    console.log('Breed description: ', data[0].description);
-  }
-});
+module.exports = {fetchBreedDescription};
 
 
-//console.log ('error:', error);
-//console.log ('statusCode:', response && response.statusCode);
-//console.log ('body: ' , body);
+// console.log ('error:', error);
+// console.log ('statusCode:', response && response.statusCode);
+// console.log ('body: ' , body);
